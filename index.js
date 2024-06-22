6a708132b64d178e5ced7f0353997f2d9ee45ff0
Student Adityaraj040 <psaditya212@gmail.com>
2024-06-22 14:51:45 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-import express from 'express'
+import express from 'express';
 import ProductController from './src/controllers/product.controller.js';
-
+import ejsLayouts from 'express-ejs-layouts';
 import path from 'path';
 
 const server = express();
 
-// setup view engine settings
+// Setup view engine settings
 server.set("view engine", "ejs");
-// path of our views
-server.set("views", path.join(path.resolve(),"src",'views')); 
+// Path of our views
+server.set("views", path.join(path.resolve(), "src", "views"));
 
-// create an instance of ProductController
+// Use express-ejs-layouts
+server.use(ejsLayouts);
+
+// Create an instance of ProductController
 const productController = new ProductController(); 
-server.get('/', (productController.getProducts));
+server.get('/', productController.getProducts);
 
+// Serve static files from the 'src/views' directory
 server.use(express.static('src/views'));
 
-server.listen(3400);
-console.log('Server is listening on port 3400');
+// Start the server
+server.listen(3400, () => {
+    console.log('Server is listening on port 3400');
+});
